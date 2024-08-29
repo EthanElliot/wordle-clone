cbe383e6acff6cb27e34741d9420a231e9c70553
squiddude38 <squiddude38@gmail.com>
2024-08-29 16:56:14 +1200

    refactored row component

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -1,33 +1,39 @@
 import { FC } from "react";
 import Box from "./Box";
 
+type CharType = {
+  value: string;
+  color: string;
+};
+
 type RowProps = {
-  guess: { value: string; color: string }[];
+  guess: CharType[];
   currentGuess?: string;
 };
 
 const Row: FC<RowProps> = ({ guess, currentGuess }: RowProps) => {
-  let boxes: React.ReactNode[] = [];
-  if (guess) {
-    boxes = guess.map(({ value, color }, i) => (
-      <Box key={i} value={value} color={color} />
-    ));
-  } else if (currentGuess) {
+  let guessArray: CharType[] = guess;
+
+  if (currentGuess) {
     const letters = currentGuess.split("");
-    boxes = [
-      ...letters.map((char, i) => <Box key={i} value={char} styles="filled" />),
-      ...Array(5 - letters.length).fill(<Box value={" "} />),
+    guessArray = [
+      ...letters.map((char) => ({ value: char, color: "" })),
+      ...Array(5 - letters.length).fill({ value: "", color: "" }),
     ];
-  } else {
-    boxes = [...Array(5).fill(<Box value={" "} />)];
+  } else if (!guess) {
+    guessArray = [...Array(5).fill({ value: "", color: "" })];
   }
+
   return (
     <div
-      className={`mb-2 flex flex-row gap-2 row ${guess ? "guessrow" : ""} ${
-        currentGuess ? "currentguessrow" : ""
-      } `}
+      className={`mb-2 flex flex-row gap-2 row
+         ${guess ? "guessrow" : ""} 
+         ${currentGuess ? "currentguessrow" : ""}
+          `}
     >
-      {boxes}
+      {guessArray.map(({ value, color }, i) => (
+        <Box key={i} value={value} color={color} />
+      ))}
     </div>
   );
 };
